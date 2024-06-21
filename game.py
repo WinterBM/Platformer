da74fba14b4349ec8da2c7189a8565885ed330f8
Bastian <b.winter01@stud.uni-goettingen.de>
2024-06-21 21:28:10 +0200

    Initial commit

---
+++ game.py
@@ -0,0 +1,53 @@
+# Libraries
+import sys
+from sprites import *
+
+# Initiaze and set constants
+pygame.init()
+
+height = 500
+width = 800
+g = 0.5
+ACC = 0.5
+fric = -0.12
+fps = 60
+
+FramesPerSec = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Game")
+
+# Read levels
+all_sprites = pygame.sprite.Group()
+object_sprites = pygame.sprite.Group()
+level_reader("Levels/level1.txt", all_sprites, object_sprites)
+
+P1 = Player()
+all_sprites.add(P1)
+# Game loop
+while True:
+    ## Get player inputs
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if pygame.sprite.spritecollide(P1, object_sprites, False):
+                    P1.jump()
+
+    displaysurface.fill((0,0,0))
+    ## Calculate forces
+    P1.move(g, ACC, fric)
+    if P1.pos.x > width*0.7:
+        for entity in object_sprites:
+            entity.move(ACC,fric, "right")
+    P1.update(object_sprites)
+
+    ## Draw sprites
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+        
+
+    pygame.display.update()
+    FramesPerSec.tick(fps)
---
+++ sprites.py
@@ -0,0 +1,111 @@
+import pygame
+from pygame.locals import *
+
+vec = pygame.math.Vector2
+
+def level_reader(path:str, all_sprites, object_sprites):
+    """
+    Reads level txt file and adds to aproriate sprite groups
+
+    :param path str: path to level txt file
+    :param all_sprites : sprite group for all sprites
+    :param object_sprites [TODO:type]: sprite group for platform sprites
+    """
+    with open(path, "r") as f:
+        lines = f.readlines()
+        for y, line in enumerate(lines):
+            for x, pos in enumerate(line):
+                if pos =="x":
+                    PT = Platform((x*50,y*50))
+                    all_sprites.add(PT)
+                    object_sprites.add(PT)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((30,30))
+        self.surf.fill((128,255,40))
+        self.rect = self.surf.get_rect(center= (40, 420))
+
+        self.pos = vec((40,420))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+
+    def move(self,g,ACC,fric):
+        """
+        Implements movement
+        """
+        self.acc = vec(0,g)
+
+        pressed_keys = pygame.key.get_pressed()
+
+        ## Acceleration events
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC
+        
+        ## Equations of motion
+        self.acc += self.vel * fric
+        self.vel += self.acc
+        self.pos += self.vel +0.5*self.acc
+        
+        self.rect.midbottom = self.pos
+
+    def update(self, sprite_group):
+        """
+        detects collisions
+        """
+        hits = pygame.sprite.spritecollide(self, sprite_group,
+                                           False)
+        if hits:
+            self.pos.y = hits[0].rect.top+1
+            self.vel.y = 0
+        
+    def jump(self):
+        """
+        implements jump
+        """
+        self.vel.y = -20
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, position):
+        super().__init__()
+
+        self.surf = pygame.Surface((50,50))
+        self.surf.fill((255,0,0))
+        self.rect = self.surf.get_rect(center= position)
+
+        self.acc = vec(0,0)
+        self.vel = vec(0,0)
+        self.pos = vec(position)
+    
+    def move(self, ACC:float, fric:float, direct: str):
+        """
+        Implements movement of platform sprites when
+        player reaches screen border
+
+        :param ACC float: acceleration
+        :param fric float: friction
+        :param direct string: left or right border
+        """
+        
+        pressed_keys = pygame.key.get_pressed()
+
+        ## Acceleration events
+        if pressed_keys[K_LEFT] and direct=="left":
+            self.acc.x = ACC
+        if pressed_keys[K_RIGHT] and direct=="right":
+            self.acc.x = -ACC
+        
+        ## Equations of motion
+        self.acc += self.vel * fric
+        self.vel += self.acc
+        self.pos += self.vel +0.5*self.acc
+        
+        self.rect.midbottom = self.pos
+
+    
+
+
