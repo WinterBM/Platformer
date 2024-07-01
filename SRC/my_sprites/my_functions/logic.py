384a18a98418a239ef7972d1added9a82ef174ff
Bastian <b.winter01@stud.uni-goettingen.de>
2024-07-01 20:13:26 +0200

    Bordermovement

--- SRC/my_sprites/my_functions/logic.py
+++ SRC/my_sprites/my_functions/logic.py
@@ -10,11 +10,13 @@ def border_move(P1, object_sprites, width:int ,ACC, fric):
     :param width int: width of display
     """
     pressed_keys = pygame.key.get_pressed()
-    if P1.pos.x < 0.2*width and pressed_keys[K_LEFT]:
+
+    if P1.pos.x < 0.2*width and P1.vel.x < 0:
         for entity in object_sprites:
-            entity.move(ACC, fric, "left")
+            entity.rect.x -= P1.vel.x+0.5*P1.acc.x
         P1.pos.x = 0.2*width
-    if P1.pos.x > 0.8*width and pressed_keys[K_RIGHT]:
+
+    if P1.pos.x > 0.8*width and P1.vel.x > 0:
         for entity in object_sprites:
             entity.rect.x -= P1.vel.x+0.5*P1.acc.x
         P1.pos.x = 0.8*width
